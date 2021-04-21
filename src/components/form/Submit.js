a6413693f41348a8abf4876d4ba3a56a58caaa1c
Syed Sarosh Farrukh <67763500+syedsaroshfarrukh@users.noreply.github.com>
2021-04-21 17:57:52 +0500

    make it better

--- src/components/form/Submit.js
+++ src/components/form/Submit.js
@@ -1,15 +1,16 @@
 import React,{useEffect} from "react";
 const Submit = ({ navigation }) => {
+  
   React.useEffect(() => {
     setTimeout(() => {
      next()
     }, 10000);
   });
-
+  
   const { next } = navigation;
   return (
     <div className="we_be">
-      <h3>Thank you for submitting. We will be in touch</h3>
+      <h3 className="we_h3_text">Vielen Dank für Ihr Vertrauen! Wir melden uns in Kürze bei Ihnen.</h3>
      
     </div>
   );
