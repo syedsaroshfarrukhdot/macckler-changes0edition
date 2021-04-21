b722f5aa8a73547c9ec34e9d882713837fabca71
Syed Sarosh Farrukh <67763500+syedsaroshfarrukh@users.noreply.github.com>
2021-04-21 19:43:59 +0500

    make it better

--- src/components/form/StepTwelve.js
+++ src/components/form/StepTwelve.js
@@ -135,7 +135,7 @@ const StepThree = ({ setForm, formData, navigation }) => {
               onClick = {async () => {
                 next()
                 await axios
-                  .post("https://backend-hausfrage-web.herokuapp.com/create-form/", formData);
+                  .post("https://eigenheim-backend.herokuapp.com/create-form/", formData);
                   
               }
             }
